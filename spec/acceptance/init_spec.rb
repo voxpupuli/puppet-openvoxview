186ca7113af939248eb57b485744cd180ca6e6d8
Sebastian Rakel <sebastian@devunit.eu>
2025-07-23 22:25:42 +0200

    Add test case - service running

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -15,6 +15,11 @@ describe 'class openvoxview:' do
       end
     end
 
+    describe service('openvoxview.service') do
+      it { is_expected.to be_enabled }
+      it { is_expected.to be_running }
+    end
+
     describe command('/usr/local/bin/openvoxview -version') do
       its(:stdout) { is_expected.to start_with latest_release }
     end
